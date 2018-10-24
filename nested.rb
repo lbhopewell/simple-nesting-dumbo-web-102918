6ac505c08e5856d5583364abc968eca1350409f9
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-24 16:33:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+hopper = programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -78,9 +79,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+ 		}
+ 		
+programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto][:know_for] = "Ruby"
 
-    
 end
 
 def changing_alan
@@ -101,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+programmer_hash    
      
 end
 
