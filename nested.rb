2adee766a16c5857e0574b12cef1fe791a5ab068
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-24 17:06:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,6 @@ def adding_matz
           :languages => ["C"]
         }
  		}
-
 programmer_hash[:yukihiro_matsumoto][:know_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 binding.pry
